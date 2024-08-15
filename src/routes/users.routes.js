e4255fec321c4c5b936d7e6d6c35f728eda1bb14
John Anderson <lyradev@proton.me>
2024-08-15 17:42:47 -0300

    update route delete /transactions

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTransaction, deleteTransaction, getAllTransactions, getTransactionById, updateTransaction } from "../controllers/transactions.services"
+import { createTransaction, deleteAllTransactions, deleteTransaction, getAllTransactions, getTransactionById, updateTransaction } from "../controllers/transactions.services"
 import { validateProductBody, validateProductIfPresent, validateTransactionParams, validateUserBody, validateUserIfPresent } from '../middlewares/global.middleware';
 export const transactionsRouter = express.Router();
 
@@ -8,4 +8,4 @@ transactionsRouter.get('/transactions/:id', validateTransactionParams, getTransa
 transactionsRouter.post('/transactions', validateUserBody, validateProductBody, createTransaction);
 transactionsRouter.patch('/transactions/:id', validateTransactionParams, validateUserIfPresent, validateProductIfPresent, updateTransaction);
 transactionsRouter.delete('/transactions/:id', validateTransactionParams, deleteTransaction);
-transactionsRouter.delete('/transactions', deleteAllStockTransactions);
+transactionsRouter.delete('/transactions', deleteAllTransactions);
